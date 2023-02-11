35e777c4b7c7afbfc1f0034b635d898fd70ad7cf
Tanjinul Hoque <68518881+Tanjinul@users.noreply.github.com>
2023-02-10 22:37:41 -0500

    Initial test of sticky navbar (it is working)

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// Sticky Nav JS
+window.onscroll = function() {stickNav()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function stickNav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
