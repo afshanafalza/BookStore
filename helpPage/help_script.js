58a19b1607d88e7d410c4e6e25ad0b2c40597a97
TimosDakis <85649514+TimosDakis@users.noreply.github.com>
2023-02-12 14:19:14 -0500

    helpPage - added ripple

--- helpPage/help_script.js
+++ helpPage/help_script.js
@@ -1,4 +1,4 @@
-/* taken / adapted from "50 Projects in 50 Days" "FAQ-collapse" and "Scroll Animation" */
+/* taken / adapted from "50 Projects in 50 Days" "FAQ-collapse", "Scroll Animation", and "Ripple Button" */
 
 const toggles = document.querySelectorAll('.faq-toggle')
 
@@ -27,3 +27,38 @@ function checkBoxes() {
         }
     })
 }
+
+const buttons = document.querySelectorAll('.ripple')
+
+buttons.forEach(button => {
+    button.addEventListener('click', function (e) {
+        const x = e.pageX
+        const y = e.pageY
+
+        const buttonLeft = e.target.offsetLeft
+        const buttonTop = e.target.offsetTop
+
+        const parentLeft = e.target.parentElement.offsetLeft
+        const parentTop = e.target.parentElement.offsetTop
+
+        let xInside = x - parentLeft - buttonLeft
+        let yInside = y - parentTop - buttonTop
+
+        // if user presses the actual icon within button, find grandparent container's offsets to subtract to get correct position for ripple
+        if(e.target.getAttribute("class") === "fas fa-chevron-down" || e.target.getAttribute("class") === "fas fa-times"){
+            const grandparentLeft = e.target.parentElement.parentElement.offsetLeft
+            const grandparentTop = e.target.parentElement.parentElement.offsetTop
+            xInside -= grandparentLeft
+            yInside -= grandparentTop
+        }
+
+        const circle = document.createElement('span')
+        circle.classList.add('circle')
+        circle.style.top = yInside + 'px'
+        circle.style.left = xInside + 'px'
+
+        this.appendChild(circle)
+
+        setTimeout(() => circle.remove(), 500)
+    })
+})
