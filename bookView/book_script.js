fe3dc6daff9db1b5b5b4d5ed45f4eaaf9e8128e2
afshanafalza <71531125+afshanafalza@users.noreply.github.com>
2023-02-18 20:45:21 -0500

    book view page

---
+++ bookView/book_script.js
@@ -0,0 +1,57 @@
+
+// Sticky Nav
+const nav = document.querySelector('.nav')
+window.addEventListener('scroll',fixNav)
+
+function fixNav(){
+  if(window.scrollY > nav.offsetHeight + 150){
+    nav.classList.add('active')
+  } else {
+    nav.classList.remove('active')
+  }
+}
+
+// Rating Button
+for(let i=1; i<=5; i++) {
+    let rate = document.querySelector(`#book-rating-${i}`);
+    let ratebox = document.querySelector(`#rating-box-${i}`);
+    let submit = document.querySelector(`#submit${i}`);
+    let close = document.querySelector(`#close${i}`);
+
+    rate.addEventListener('click', () => {
+        ratebox.classList.remove("hidden");
+    });
+    
+    // submit1.addEventListener('click', () => {
+    //     console.log("clicked");
+    //     // ratebox.classList.remove("hidden");
+    // });
+}
+
+// Categories Button
+let catbutton = document.querySelector(`#cat-container`);
+let catArea = document.querySelector(`.categories`);
+let clickCount = 0;
+console.log(catArea);
+
+catbutton.addEventListener('click', () => {
+    if(clickCount%2==0) {
+        console.log("clicked");
+        catArea.classList.remove("hidden");
+    }
+    else {
+        console.log("clicked");
+        catArea.classList.add("hidden");    
+    }
+    clickCount++;    
+});
+
+// Search
+const search = document.querySelector(`.search`);
+const btn = document.querySelector(`.btn`);
+const input = document.querySelector(`.input`);
+
+btn.addEventListener('click', () => {
+    search.classList.toggle('active');
+    input.focus();
+})
